bb7fa1b2e1508b3e9bc05ac1af114e22ec0b4e79
Benjamin <benjamin.gallean@etu.umontpellier.fr>
2018-12-16 14:19:27 +0100

    exlposifs

---
+++ Assets/Explosif.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosif : MonoBehaviour
+{
+    public int radius;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject[] presents = GameObject.FindGameObjectsWithTag("presents");
+        foreach(GameObject c in presents)
+        {
+            if ((c.gameObject.transform.position - gameObject.transform.position).magnitude <= radius)
+            {
+                Destroy(c);
+            }
+        }
+        Destroy(this);
+    }
+}
