dcc98478d1d48e3a4c05b48ed4b4d5012c47388e
Foopyo <romain.chamoulaud@gmail.com>
2018-12-16 12:37:31 +0100

    Ajout collider trigger chemine/cadeau

---
+++ Assets/Scripts/Cadeau.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cadeau : MonoBehaviour
+{
+    public int idChemine;
+    private List<GameObject> allAdjacent;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        idChemine = -1;
+        allAdjacent = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8) //Si c'est une cheminée
+        {
+            idChemine = collision.gameObject.GetComponent<Cheminee>().id;
+        }
+        if (collision.gameObject.layer == 9 && collision.gameObject.GetComponent<Cadeau>().getCheminee()!=-1) //Si c'est un cadeau qui sera aspiré
+        {
+            idChemine = collision.gameObject.GetComponent<Cadeau>().getCheminee();
+            allAdjacent.Add(collision.gameObject);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+            idChemine = -1;
+        else
+        {
+            allAdjacent.Remove(collision.gameObject);
+            idChemine = -1;
+            foreach(GameObject go in allAdjacent)
+            {
+                int id = go.GetComponent<Cadeau>().getCheminee();
+                if (id != -1)
+                {
+                    idChemine = id;
+                }
+            }
+        }
+    }
+
+    public int getCheminee()
+    {
+        return idChemine;
+    }
+}
